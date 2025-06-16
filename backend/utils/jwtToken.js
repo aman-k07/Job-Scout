3a172b2cbe6e4e45dd6e629a1fe01d9c136fc589
aman-k07 <aman2436411@gmail.com>
2025-06-17 01:59:41 +0530

    samesite attribute added to cookies

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,6 +5,8 @@ export const sendToken = (user, statusCode, res, message) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true, // Set httpOnly to true
+    secure: process.env.NODE_ENV === "production" ? true : false,
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
   };
 
   res.status(statusCode).cookie("token", token, options).json({
