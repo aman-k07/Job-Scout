06d3aacbb7036ab5b8a78ceb7a2d0c49c269cbf7
aman-k07 <aman2436411@gmail.com>
2025-06-17 01:03:28 +0530

    api hardcoded url moved to env

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,13 +17,14 @@ import PostJob from "./components/Job/PostJob";
 import NotFound from "./components/NotFound/NotFound";
 import MyJobs from "./components/Job/MyJobs";
 
+
 const App = () => {
   const { isAuthorized, setIsAuthorized, setUser } = useContext(Context);
   useEffect(() => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/getuser",
+          import.meta.env.VITE_API_URL + "/api/v1/user/getuser",
           {
             withCredentials: true,
           }
